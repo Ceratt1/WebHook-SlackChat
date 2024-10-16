e1bcb7cafb2d03f775442b8d567c521d4b33e596
Gabriel Cabral <gabriel.cabral@otisoftware.com.br>
2024-10-16 12:53:41 -0300

    formatacao

--- sendMessage.js
+++ sendMessage.js
@@ -15,20 +15,20 @@ Here’s our Meta Ads performance snapshots:
 ⚡${formattedDate}⚡
 
 -----------------------------
-Total Spend: $${data.spend.toFixed(2).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
-Total Revenue: $${data.action_values_omni_purchase.toFixed(2).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+Total Spend: $${Number(data.spend).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+Total Revenue: $${Number(data.action_values_omni_purchase).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
 ROA: ${data.website_purchase_roas_offsite_conversion_fb_pixel_purchase.toFixed(1)}x
 AOV: $${data.AOV.toFixed(2)}
 CR : ${data.CR.toFixed(2)}%
 -----------------------------
 Total Purchases: ${data.actions_offsite_conversion_fb_pixel_purchase}
 Cost Per Purchase: $${Number(data.cost_per_action_type_omni_purchase).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
-Total Clicks: ${data.link_clicks}
-Cost Per Click: $${data.cost_per_action_type_link_click.toFixed(2).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}.00
-Add To Cart: ${data.actions_add_to_cart}
+Total Clicks: ${Number(data.link_clicks).toLocaleString('en-US', { minimumFractionDigits: 0 })}
+Cost Per Click: $${data.cost_per_action_type_link_click.toFixed(2).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+Add To Cart: ${Number(data.actions_add_to_cart).toLocaleString('en-US', { minimumFractionDigits: 2 })}
 Cost Per Add To Cart: $${data.cost_per_action_type_add_to_cart.toFixed(2).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
-Total Initiate Checkouts: $${data.actions_initiate_checkout.toFixed(2).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
-Cost Per Initiate Checkout: $${data.cost_per_action_type_initiate_checkout.toFixed(2).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}.00
+Total Initiate Checkouts: $${Number(data.actions_initiate_checkout).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+Cost Per Initiate Checkout: $${Number(data.cost_per_action_type_initiate_checkout).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
 Click Through Rate: ${data.outbound_clicks_ctr_outbound_click.toFixed(2)}%
   `
 
